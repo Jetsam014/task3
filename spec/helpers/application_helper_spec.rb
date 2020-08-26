ee0caff398e73a1246da3064410547cf7454610a
Flotsam <neroexplosionalbatrossblindfold@yahoo.co.jp>
2020-08-27 03:45:44 +0900

    Product detail page　create

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe "Applibation title helper" do
+    it "displayed full title" do
+      expect(full_title("title")).to eq "title - BIGBAG Store"
+      expect(full_title("")).to eq "BIGBAG Store"
+    end
+  end
+end
