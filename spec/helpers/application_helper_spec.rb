11e000062b052954b8ca429f9ec15e2f7a5bf453
Flotsam <neroexplosionalbatrossblindfold@yahoo.co.jp>
2020-09-01 23:32:27 +0900

    Indent correction&Request correction

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe "Applibation title helper" do
-    it "displayed full title" do
+    it "引数がない場合" do
       expect(full_title("title")).to eq "title - BIGBAG Store"
+    end
+    it "引数が空の場合" do
       expect(full_title("")).to eq "BIGBAG Store"
+    end
+    it "引数がnilの場合" do
       expect(full_title(nil)).to eq "BIGBAG Store"
     end
   end
--- spec/request/products_spec.rb
+++ spec/request/products_spec.rb
@@ -7,8 +7,17 @@ RSpec.describe "Products", type: :request do
   describe "GET/potepan/products/id" do
     before { get potepan_product_path(product.id) }
 
-    it "request succeeded" do
+    it "２００レスポンスを返す" do
       expect(response.status).to eq 200
+    end
+    it "商品名が表示される" do
+      expect(response.body).to include product.name
+    end
+    it "商品価格が表示される" do
+      expect(response.body).to include product.price.to_s
+    end
+    it "showテンプレートが表示される" do
+      expect(response).to render_template :show
     end
   end
 end
