0a3fa6c6d33cd0ae7aad0c410c2c8b55f155f148
Flotsam <neroexplosionalbatrossblindfold@yahoo.co.jp>
2020-09-03 01:22:18 +0900

    spec/rails_helper.rb Rewriting

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe "Applibation title helper" do
-    it "引数がない場合" do
+    it "タイトルが与えられた場合" do
       expect(full_title("title")).to eq "title - BIGBAG Store"
     end
-    it "引数が空の場合" do
+    it "タイトルが空の場合" do
       expect(full_title("")).to eq "BIGBAG Store"
     end
-    it "引数がnilの場合" do
+    it "タイトルがnilの場合" do
       expect(full_title(nil)).to eq "BIGBAG Store"
     end
   end
