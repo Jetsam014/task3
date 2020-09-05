a3f039904b2493c29e2033f9483106f811980d77
Flotsam <neroexplosionalbatrossblindfold@yahoo.co.jp>
2020-09-05 20:06:58 +0900

    Constantization application_helper BaseTitle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
+  Base_title = "BIGBAG Store".freeze
   def full_title(page_title)
-    base_title = "BIGBAG Store"
     if page_title.blank?
-      base_title
+      Base_title
     else
-      "#{page_title} - #{base_title}"
+      "#{page_title} - #{Base_title}"
     end
   end
 end
